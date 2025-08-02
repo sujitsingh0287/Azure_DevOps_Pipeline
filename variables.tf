c145d7e31d08380a862fe56a55a61223e5e903e9
Sujit Singh <sujitsingh0287@gmail.com>
2025-08-02 22:39:00 +0530

    added yml block10

--- variables.tf
+++ variables.tf
@@ -30,19 +30,19 @@ variable "subnet" {
 }
 
 
-variable "client_id" {
-  type = string
-}
+# variable "client_id" {
+#   type = string
+# }
 
-variable "client_secret" {
-  type = string
-  sensitive = true
-}
+# variable "client_secret" {
+#   type = string
+#   sensitive = true
+# }
 
-variable "tenant_id" {
-  type = string
-}
+# variable "tenant_id" {
+#   type = string
+# }
 
-variable "subscription_id" {
-  type = string
-}
+# variable "subscription_id" {
+#   type = string
+# }
