62830753e8821f91ce7cef1b49f0cb28d42838a4
Sujit Singh <sujitsingh0287@gmail.com>
2025-08-05 20:23:02 +0530

    added yml block15

--- variables.tf
+++ variables.tf
@@ -1,48 +1,48 @@
-# variable "dev_stg" {
-#   type = string
-# }
+variable "dev_stg" {
+  type = string
+}
 
-# variable "rg_name" {
-#   type = string
-# }
+variable "rg_name" {
+  type = string
+}
 
-# variable "location" {
-#   type = string
-# }
+variable "location" {
+  type = string
+}
 
-# variable "vnet_name" {
-#   type = string
+variable "vnet_name" {
+  type = string
 
-# }
+}
 
-# variable "vm_name" {
-#   type = string
-# }
+variable "vm_name" {
+  type = string
+}
 
-# # variable "subnet_id" {
-# #   type = string
-
-# # }
-
-# variable "subnet" {
+# variable "subnet_id" {
 #   type = string
 
 # }
 
+variable "subnet" {
+  type = string
 
-# # variable "client_id" {
-# #   type = string
-# # }
+}
 
-# # variable "client_secret" {
-# #   type = string
-# #   sensitive = true
-# # }
 
-# # variable "tenant_id" {
-# #   type = string
-# # }
+# variable "client_id" {
+#   type = string
+# }
 
-# # variable "subscription_id" {
-# #   type = string
-# # }
+# variable "client_secret" {
+#   type = string
+#   sensitive = true
+# }
+
+# variable "tenant_id" {
+#   type = string
+# }
+
+# variable "subscription_id" {
+#   type = string
+# }
