89f3990db0d6ac225a2acbce6c5dc0cb7eb67384
sujitsingh0287 <sujitsingh0287@gmail.com>
2025-09-03 01:59:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,4 +26,22 @@ variable "subnet" {
 
 }
 
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+  sensitive   = true
+}
+
 
