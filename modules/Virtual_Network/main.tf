798d9a4bf0a5aa00aa937a3296786c0b417b9b6e
sujitsingh0287 <sujitsingh0287@gmail.com>
2025-09-04 09:42:25 +0530

    Update main.tf

--- modules/Virtual_Network/main.tf
+++ modules/Virtual_Network/main.tf
@@ -12,5 +12,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name = var.rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes = ["10.0.1.0/24"]
+depends_on = [azurerm_virtual_network.vnet]
   
 }
