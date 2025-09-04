7f8d2bb2d0fc05e718485045f258f637efe33874
Sujit Singh <sujitsingh0287@gmail.com>
2025-09-04 09:17:23 +0530

    modify provider block1

--- providers.tf
+++ providers.tf
@@ -7,18 +7,18 @@ terraform {
   }
 }
 
-#   backend "azurerm" {
-#     resource_group_name  = "RG-TFSTATEFILE-DONNOT-DELETE"
-#     storage_account_name = "securetfstatefile"
-#     container_name       = "tfstatefile"
-#     key                  = "terraform.tfstate"
-#   }
-# }
+  backend "azurerm" {
+    resource_group_name  = "RG-TFSTATEFILE-DONNOT-DELETE"
+    storage_account_name = "securetfstatefile"
+    container_name       = "tfstatefile"
+    key                  = "terraform.tfstate"
+  }
+
 
 provider "azurerm" {
   features {}
-#   use_oidc = true
-#   subscription_id = var.subscription_id
-#   tenant_id       = var.tenant_id
-#   client_id       = var.client_id
+  use_oidc = true
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
 }
